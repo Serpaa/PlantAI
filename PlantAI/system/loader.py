092ec4dfb1d968d2fe5621fb883723c9a9ec7d8e
Serpaa <tim.grundey@t-online.de>
2025-11-26 13:58:25 +0100

    Move config to resources

--- PlantAI/system/loader.py
+++ PlantAI/system/loader.py
@@ -9,6 +9,6 @@ import yaml
 
 def getConfig(dir1: str, dir2: str, data: str):
     """Returns a value stored in the config file under a certain path."""
-    with open("PlantAI/system/config.yaml") as stream:
+    with open("PlantAI/resources/config.yaml") as stream:
         config = yaml.safe_load(stream)
         return config[dir1][dir2][data]
