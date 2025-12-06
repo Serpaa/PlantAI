6d15dfc36a807634b18dab3c4156f4f50c389f66
Serpaa <tim.grundey@t-online.de>
2025-12-06 17:47:55 +0100

    Add instructions to use full words for numbers

--- PlantAI/interface/llm.py
+++ PlantAI/interface/llm.py
@@ -6,10 +6,8 @@ Created: 26.11.2025
 """
 
 import logging
-import re
 from ollama import chat
 from ollama import ChatResponse
-from num2words import num2words
 
 def question(prompt: str, data) -> str:
     """
@@ -25,8 +23,9 @@ def question(prompt: str, data) -> str:
     # Build system message with rules
     sysContent = (
         "System rules (follow these strictly):"
-            "- You are a voice assistant."
+            "- You are a helpful voice assistant."
             "- Answer in exactly one short sentence."
+            "- Always use full words for numbers e.g. three, seven or sixty."
             "- No additional comments.")
     
     # Build user message
@@ -45,5 +44,5 @@ def question(prompt: str, data) -> str:
     logging.info(f"Prompt: {userContent}")
     logging.info(f"Response created: {response.message.content}")
 
-    # Convert all numbers to words and return
-    return re.sub(r'\d+', lambda m: num2words(int(m.group())), response.message.content)
+    # Return response
+    return response.message.content
