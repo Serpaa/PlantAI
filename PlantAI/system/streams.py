ee2b11e908c0f74676ee497462e24f222cdfeb51
Serpaa <tim.grundey@t-online.de>
2025-12-20 15:54:09 +0100

    Adjust error handler

--- PlantAI/system/streams.py
+++ PlantAI/system/streams.py
@@ -106,9 +106,11 @@ _c_error_handler = None
 
 def muteALSA():
     global _c_error_handler
-    
+
     ERROR_HANDLER_FUNC = ctypes.CFUNCTYPE(
         None,
+        ctypes.c_int,
+        ctypes.c_int,
         ctypes.c_char_p,
         ctypes.c_int,
         ctypes.c_char_p,
