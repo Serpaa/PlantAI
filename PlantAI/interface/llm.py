1e5f90d634ce480798b72b60e64788819872b686
Serpaa <tim.grundey@t-online.de>
2025-12-20 16:55:10 +0100

    Use llama3.2:1b

--- PlantAI/interface/llm.py
+++ PlantAI/interface/llm.py
@@ -34,7 +34,7 @@ def question(prompt: str, data) -> str:
         userContent = f"'{prompt}' using additional information: '{data}'"
 
     # Send message to model
-    response: ChatResponse = chat(model='llama3.2', messages=[
+    response: ChatResponse = chat(model='llama3.2:1b', messages=[
         {'role': 'system', 'content': sysContent},
         {'role': 'user', 'content': userContent}
     ])
